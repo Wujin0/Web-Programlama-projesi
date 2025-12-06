1a53f6a456115e26b44c22910e95bae92b726ac1
Wujin0 <kabaogluertugrul@gmail.com>
2025-12-06 22:06:20 +0300

    temel yapı oluşturuldu

---
+++ WEB PROGRAMLAMA PROJESİ/WEB PROGRAMLAMA PROJESİ/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_PROGRAMLAMA_PROJESİ.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
